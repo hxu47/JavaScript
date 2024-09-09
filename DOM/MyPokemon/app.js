cc20432c8a5a892858f189c4e55f9fa60396f804
hxu47 <huidongxu1222@gmail.com>
2024-09-09 12:53:45 -0300

    add DOM

---
+++ DOM/MyPokemon/app.js
@@ -0,0 +1,24 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector("#container");
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')  //defined in app.css
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`;
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
+
+/*
+<div>
+    <img></img>  // the actual image
+    <span>2</span> // the index of the image
+</div>
+*/
